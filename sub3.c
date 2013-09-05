f52f22fcbc8955e905d04df0d1cf948c20243b18
c-mil <shiba_atsu_111@yahoo.co.jp>
2013-09-06 01:52:06 +0900

    first

---
+++ sub3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+struct AA{			//構造体
+	int month;		//月
+	int day;	
+	//日付
+	char item[100];	//内訳
+	long value;		//値段
+	int category;	//カテゴリ
+}aa[500];
+
+
+	int main(){
+		example();
+	}
+
+
+
+
+
+	int example(){
+
+
+		~~~~~
+		~~~~~
+		~~~~~
+
+	}
